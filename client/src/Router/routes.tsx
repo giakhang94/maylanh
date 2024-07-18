21bc3ee6605d24c4ffac7c3072751b57a7df005d
Trung Tran <trung.tran@asicompany.asia>
2024-07-18 21:00:59 +0700

    feat: FE#2 - add routes constants

---
+++ client/src/Router/routes.tsx
@@ -0,0 +1,8 @@
+export const HOME = "/";
+
+export const ADMIN_ORDER = "/admin/order";
+export const ADMIN_LIST_SERVICE = "/admin/all-services";
+export const ADMIN_ADD_SERVICE_PAGE = "/admin/add-service";
+
+export const LOGIN_PAGE = "/login";
+export const REGISTER_PAGE = "/register";
